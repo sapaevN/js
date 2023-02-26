998d8e25f2d5f898919048b19ff056279622fd8f
sapaev_n <snu5207824@icloud.com>
2023-02-26 23:47:37 +0300

    Упражнение по написанию кода 8(*) числа фибоначчи:

--- script/script.js
+++ script/script.js
@@ -1,42 +1,19 @@
 "use strict";
+//числа фибоначчи 
+let num  = 20;
 
-// let num = 111;
-// function getTimeFromMinutes(minutes){
-// 	if(minutes < 0 || typeof(minutes) != 'number'){
-// 		return console.log('Ошибка, проверьте данные');
-// 	}
-// 		let result = '';
-// 		let calc = minutes / 60;
-// 		calc = parseInt(calc);
-// 		let remainder = minutes % 60;
-// 		if(calc === 1){
-// 			result = `это ${calc} час и ${remainder} минут`
-// 			return console.log(result);
-// 		}else if(calc > 1 && calc < 5 ){
-// 			result = `это ${calc} часa и ${remainder} минут`
-// 			return console.log(result);
-// 		}
-// 		else{
-// 			result = `это ${calc} часов и ${remainder} минут`
-// 			return console.log(result);
-// 		}
-// }
-// getTimeFromMinutes(num);
-	
-		let first = 90,
-			 second = 40,
-			 third = 41,
-			 last = 4;
-	
-
-function findMaxNumber(a,b,c,d){
-	const arr = [a,b,c,d];
-	let max = arr[0];
-	for(let i = 0; i < arr.length; i++){
-		if(max < arr[i]){
-			max = arr[i]
-		}
+function fib(fibNum){
+	let result = '';
+	if(typeof(fibNum) != 'number'){
+		return console.log(result) ;
 	}
-	return console.log(max);
+	let arr = [0,1];
+	result = `${arr[0]} ${arr[1]} `
+	for(let i = 2; i<fibNum; i++){
+		arr[i] = arr[i-1] + arr[i-2];
+		result = result + `${arr[i] = arr[i-1] + arr[i-2]} `;
+	}
+	return console.log(result);
 }
-findMaxNumber(first,second, third,last);
+
+fib(num);
