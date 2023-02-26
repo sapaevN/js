f68bcf9293053feeaf4b6854c997295d3cf756d0
sapaev_n <snu5207824@icloud.com>
2023-02-27 00:16:23 +0300

    метод трим

--- script/script.js
+++ script/script.js
@@ -1,19 +1,13 @@
 "use strict";
-//числа фибоначчи 
-let num  = 20;
+let a;
+rememberFilms()
 
-function fib(fibNum){
-	let result = '';
-	if(typeof(fibNum) != 'number'){
-		return console.log(result) ;
-	}
-	let arr = [0,1];
-	result = `${arr[0]} ${arr[1]} `
-	for(let i = 2; i<fibNum; i++){
-		arr[i] = arr[i-1] + arr[i-2];
-		result = result + `${arr[i] = arr[i-1] + arr[i-2]} `;
-	}
-	return console.log(result);
+
+function rememberFilms(){
+	let a = prompt("последний просмотренный фильм", "").trim()
+	const moviesDB = {
+		lastFilms: a
+	};
+	console.log(moviesDB);
 }
 
-fib(num);
