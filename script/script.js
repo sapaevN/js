4fd01327a767e45e44bb46f9263fca8a3efe41da
sapaev_n <snu5207824@icloud.com>
2023-02-27 16:49:27 +0300

    остановлено

--- script/script.js
+++ script/script.js
@@ -1,25 +1,54 @@
-"use strict";
+// "use strict";
 
-function first(){
-	//do something
-	setTimeout(function(){
-		console.log(1)
-	},1500);
-}
+// const options = {
+// 	name: 'test',
+// 	width: 1024,
+// 	height: 1024,
+// 	colors: {
+// 		border: "black",
+// 		bg: "red"
+// 	},
+// 	makeTest: function (){
+// 		console.log("Test")
+// 	}
+ 
+// }
+// const {border,bg} = options.colors;
+// console.log(border)
 
-function second(){
-	console.log(2);
-}
 
-first();
-second();
 
-function learnJs(lang, callback){
-	console.log(`я учу ${lang}`);
-	callback();
-}
-function done(){
-	console.log('я прошёл этот урок')
-}
 
-learnJs('JS',done);
+// console.log(options);
+
+// delete options.name;
+// console.log(options)
+// let counter = 0;
+
+// for (let key in options){
+// 	if(typeof(options[key])==='object'){
+// 		for (let i in options[key] ){
+// 		console.log(` свойство ${i} имеет значения ${options[key][i]}`)
+		
+// 		}
+// 	}else{
+// 		console.log(` свойство ${key} имеет значения ${options[key]}`)
+// 		counter++
+// 	}
+	
+// }
+// console.log(counter)
+
+// console.log(Object.keys(options).length);
+
+// let counter = 0;
+// for (let key in options){
+// 	if(typeof(options[key] === 'object')){
+// 		for (let i in options[key]){
+// 			console.log(options[key][i])
+// 		}
+// 	}
+// 	 console.log(`key  ${key} - value ${options[key]}`);
+// 	counter++;
+// }
+// console.log(`object options have ${counter} keys`)
