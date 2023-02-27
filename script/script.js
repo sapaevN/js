39a1cf414e1724ab039eec6c1a376fd62c415c8d
sapaev_n <snu5207824@icloud.com>
2023-02-27 13:40:51 +0300

    31. Callback- функции

--- script/script.js
+++ script/script.js
@@ -1,13 +1,25 @@
 "use strict";
-let a;
-rememberFilms()
 
-
-function rememberFilms(){
-	let a = prompt("последний просмотренный фильм", "").trim()
-	const moviesDB = {
-		lastFilms: a
-	};
-	console.log(moviesDB);
+function first(){
+	//do something
+	setTimeout(function(){
+		console.log(1)
+	},1500);
 }
 
+function second(){
+	console.log(2);
+}
+
+first();
+second();
+
+function learnJs(lang, callback){
+	console.log(`я учу ${lang}`);
+	callback();
+}
+function done(){
+	console.log('я прошёл этот урок')
+}
+
+learnJs('JS',done);
