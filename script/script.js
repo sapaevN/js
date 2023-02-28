9785b1ac495067b30274c1419906e64dcedd2462
sapaev_n <snu5207824@icloud.com>
2023-03-01 02:14:15 +0300

    Упражнение по написанию кода 11: Задачи на работу с массивами

--- script/script.js
+++ script/script.js
@@ -1,61 +1,27 @@
-"use strict";
+"use strict"
+const family = ['Peter', 'Ann', 'Alex', 'Linda']
 
-const personalPlanPeter = {
-	name: "Peter",
-	age: "29",
-	skills: {
-		languages: ["ru, eng"],
-		programmingLangs:{
-			js: "20%",
-			php: "10%"
-		},
-		exp: "1 month"
-	},
-
-	showAgeAndLangs: function(plan){
-		let result = '';
-		let myAge = plan.age;
-		let knowLengs = plan.skills.languages;
-		result += `Мне ${myAge} я владею языками: ${knowLengs.join(" :::").toUpperCase()}`
-		return console.log(result);
+function showFamily(f){
+	let resultStr = '';
+	if(f.length == 0){
+		return console.log("Семья пуста");
 	}
-
-};
-
-personalPlanPeter.showAgeAndLangs(personalPlanPeter);
-
-
-function showProgrammingLangs(plan){
-	for(let key in plan){
-		if(typeof(plan[key] === 'object')){
-			for (let i in plan[key]){
-				if(i == "programmingLangs" ){
-					for(let k in plan[key][i]){
-						if(k === "js"){
-							console.log(`Язык js изучен на ${plan[key][i][k]}`)
-						}
-						else if(k === "php"){
-							console.log(`Язык php изучен на ${plan[key][i][k]}`)
-						}else if(typeof(plan[key][i][k]) === 'undefined'){
-							console.log("")
-						}
-					}
-				}
-			}
-		}
+	else{
+		f.forEach(function(e, n, link){
+			resultStr += `${e} `;
+		});
 	}
+	return console.log(resultStr);
+}
+showFamily(family);
+const itemcities = ['LisBon', 'ROME', 'milan', 'DubLin'];
+
+function standardizStr(cities){
+	let showCities = '';
+	cities.forEach(function(k){
+		showCities += `${k} \n`;
+	});
+	return  console.log(showCities.toLowerCase());
 }
 
-function showExperience(plan){
-	for(let key in plan){
-		if(typeof(plan[key]) === "object"){
-			for(let i in plan[key]){
-				if(i == 'exp') {console.log(plan[key][i])}
-				
-			}
-		}
-	}
-}
-
-showExperience(personalPlanPeter);
-showProgrammingLangs(personalPlanPeter);
+standardizStr(itemcities);
