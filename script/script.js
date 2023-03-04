e6e1a0eb6a18cb65b64fcdbcf9201fa7d0f112bb
sapaev_n <snu5207824@icloud.com>
2023-03-05 01:09:02 +0300

    41. Задачи с собеседований на понимание основ

--- script/script.js
+++ script/script.js
@@ -1,21 +1,23 @@
 "use strict";
 
+// let x = 5; console.log(x++) 
+// console.log(([] + false - null + true)) //NaN
 
-function counter() {
-	let count = 0;
-	return function () {
-		count++;
-		return count
-	}
-}
+// let y = 1; let x = y = 2; console.log(x) //2
 
-const value = counter();
-console.log(value())
-console.log(value())
-console.log(value())
-console.log(value())
-console.log(value())
-console.log(value())
+// console.log([] + 1 + 2) //12
 
+// console.log('3123123'[0]) //3
 
+// console.log(2 && 1 && null && 0 && undefined); //null
+
+// console.log(null || 2 && 3 || 4); //3
+
+// const a = [1,2,3],b = [1,2,3]; a == b? console.log("yes"): console.log("no"); //no
+
+// console.log(+"Infinity") // Infinity 
+
+// console.log("Ёжик" > "яблоко"); //false
+
+// console.log(0 || '' || 2 || undefined || true || false); 
 
