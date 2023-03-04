4a25623471d947e40c29634c768d20f4bbef2f21
sapaev_n <snu5207824@icloud.com>
2023-03-04 15:26:45 +0300

    39. Динамическая типизация в JS

--- script/script.js
+++ script/script.js
@@ -1,48 +1,12 @@
 "use strict";
+let num = 5;
+console.log("https://vk.com/catalog/" + num);
 
-const shoppingMallData = {
-	shops: [
-		{
-			width: 10,
-			length: 5
-		},
-		{
-			width: 12,
-			length: 7
-		},
-		{
-			width: 20,
-			length: 5
-		},
-		{
-			width: 8,
-			length: 10
-		}
-	],
-	height: 5,
-	moneyPer1m3: 30,
-	budget: 50000
+const fontSize = 26 + "px";
+
+let switcher = null;
+
+if (switcher) {
+	console.log("working...")
 }
-
-function isBudgetEnough(data) {
-	let calcShop = [];
-	let calcShops = 0;
-	let square
-	for (let i = 0; i < data.shops.length; i++) {
-		let shop = Object.values(data.shops[i]);
-		calcShop[i] = shop[0] * shop[1];
-		calcShops += calcShop[i];
-	}
-	square = data.height * calcShops * data.moneyPer1m3;
-	console.log(square)
-
-	if (square > data.budget) return console.log("Бюджета недостаточно");
-	else return console.log('Бюджета достаточно');
-}
-isBudgetEnough(shoppingMallData)
-
-
-
-// let shop = Object.values(shoppingMallData.shops[3]);
-// 		let calcShops = shop[0]*shop[1];
-// 		console.log(calcShops)
+console.log(!!0)
