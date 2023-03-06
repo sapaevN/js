d8a4056954eefec52d92ff14b25ff38ca701eff6
sapaev_n <snu5207824@icloud.com>
2023-03-06 16:52:36 +0300

    46. Навигация по DOM - элементам, data-атрибуты, преимущество for/of

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 "use strict";
-const wrapper = document.querySelector('.wrapper')
-const btn = document.querySelector('button')
-const menu = document.createElement("nav");
-menu.classList.add("menu")
-menu.style = "width: 100px; height: 50px; background-color:tomato; margin-top: 30px;"
-document.querySelector('button').after(menu)
-// btn.onclick = function(){
-//     console.log('Click')
-// };
-let i = 0;
-const menuOpen = function(e){
-    
-   menu.classList.toggle('_active')
-   i++;
-   if(i == 100){
-      btn.removeEventListener('click', menuOpen);
-   }
-}
+// console.log(document.documentElement)  
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.body.childNodes)
+//console.log(document.body.firstElementChild)
+// console.log(document.body.lastChild)
 
-btn.addEventListener('click', menuOpen);  
+// console.log(document.querySelector("h1").parentNode.parentElement);
+// console.log(document.querySelector('[data-first-item]').nextElementSibling );
+// console.log(document.querySelector('[data-first-item]').previousElementSibling);
+
+// for(let node of document.body.childNodes){
+//    if(node.nodeName == "#text"){
+//       continue;
+//    }
+//    else{
+//       console.log(node)
+//    }
+// }
