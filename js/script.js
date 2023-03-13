20fc5814bb6f55c4f4edd8a4766bcddce54537d4
sapaev_n <snu5207824@icloud.com>
2023-03-13 15:18:15 +0300

    65. Скрипты и время их выполнения. setTimeout и setInterval

--- js/script.js
+++ js/script.js
@@ -1,15 +1,46 @@
 "use strict";
 
+// function logger() {
+//    i++
+//    if (i == 3) {
+//       clearInterval(timeId)
+//    }
+//    console.log("hello")
 
-// const p = document.querySelectorAll('p');
-// console.log(p)
 
-function scriptLoaded(src){
-   const script = document.createElement('script')
-   script.src = src;
-   script.async = false;
-   document.body.append(script);
+// }
+// let i = 0;
+// let timeId;
+// const btn = document.querySelector('.qvadrant');
+
+// const id = setTimeout(function log() {
+//    console.log('hello')
+//    id = setTimeout(log,500)
+// },500)
+
+
+
+let btn = document.querySelector("button")
+let target = document.querySelector(".qvadrant")
+
+
+
+
+function myAnimation() {
+
+   let pos = 0;
+
+   let id = setInterval(function () {
+
+      if (pos == 392) {
+         clearTimeout(id)
+      } else {
+        
+         pos++;
+         target.style.top = pos + "px";
+         target.style.left = pos + "px";
+      }
+   }, 0)
 }
 
-scriptLoaded("js/task.js")
-scriptLoaded("js/some.js")
+btn.addEventListener("click", myAnimation)
